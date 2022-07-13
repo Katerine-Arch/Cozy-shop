4d21e27170b8e98eaa8b02033ed6c7e228544215
Kate_Style <e.ivanova.93@bk.ru>
2022-07-13 23:00:26 +0300

    fin

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-let button = document.getElementById('header__nav-button');
-let navigation = document.getElementById('menu');
-let wrp = document.getElementById('wrp');
-button.addEventListener('click', function () {
-    navigation.classList.toggle('display-block');
-    wrp.classList.toggle('display-block');
-});
-
